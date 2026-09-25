dc132e5b3c64390ae4f99f79c9da2bea52fbaefd
Himanshu Mamgain <89596385+himanshu-mamgain@users.noreply.github.com>
2026-09-25 18:38:41 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        function isAlphaNum(c) {
+            return (c >= "a" && c <= "z") || 
+            (c >= "A" && c <= "Z") || 
+            (c >= "0" && c <= "9");
+        }
+
+        let left = 0, right = s.length - 1;
+
+        while (left < right) {
+            while (left < right && !isAlphaNum(s[left])) left++;
+            while (left < right && !isAlphaNum(s[right])) right--;
+
+            if (s[left].toLowerCase() !== s[right].toLowerCase()) return false;
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
